df6637d34125dc7258d3b103f7a6d387a965f025
DRDosSAntos <dr.dos.santos@belastingdienst.nl>
2021-04-26 14:51:03 +0200

    geen dubbele bezorgwijzes

--- src/main/java/nl/belastingdienst/registreren/BezorgwijzeDAO.java
+++ src/main/java/nl/belastingdienst/registreren/BezorgwijzeDAO.java
@@ -4,33 +4,43 @@ import lombok.Data;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import java.util.List;
+
 import static nl.belastingdienst.registreren.RegistratieApp.em;
 
 public class BezorgwijzeDAO {
 
-   public void initData(){
+    public void initData() {
+        TypedQuery<Bezorgwijze> q = em.createQuery("SELECT b FROM Bezorgwijze b", Bezorgwijze.class);
+        if (q.getResultList().size() == 4) {
+            return;
+        }
 
-       Bezorgwijze afhalen = new Bezorgwijze();
-       afhalen.setId(1);
-       afhalen.setNaam("Afhalen");
-       persist(afhalen);
 
-       Bezorgwijze verzenden = new Bezorgwijze();
-       verzenden.setId(2);
-       verzenden.setNaam("Verzenden");
-       persist(verzenden);
+        Bezorgwijze afhalen = new Bezorgwijze();
+        afhalen.setId(1);
+        afhalen.setNaam("Afhalen");
+        persist(afhalen);
 
-       Bezorgwijze magazijn = new Bezorgwijze();
-       magazijn.setId(3);
-       magazijn.setNaam("Magazijn");
-       persist(magazijn);
+        Bezorgwijze verzenden = new Bezorgwijze();
+        verzenden.setId(2);
+        verzenden.setNaam("Verzenden");
+        persist(verzenden);
 
-       Bezorgwijze rembours = new Bezorgwijze();
-       rembours.setId(4);
-       rembours.setNaam("Rembours");
-       persist(rembours);
+        Bezorgwijze magazijn = new Bezorgwijze();
+        magazijn.setId(3);
+        magazijn.setNaam("Magazijn");
+        persist(magazijn);
 
-   }
+        Bezorgwijze rembours = new Bezorgwijze();
+        rembours.setId(4);
+        rembours.setNaam("Rembours");
+        persist(rembours);
+
+    }
 
     private void persist(Bezorgwijze b) {
         em.getTransaction().begin();
